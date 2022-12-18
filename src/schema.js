972e2020b35fd955d7e1497c2d79fab8115bb473
Dmitry Ilyin <mowgle>
2022-12-18 22:36:49 +0300

    feat: add scalar DateTime

--- src/schema.js
+++ src/schema.js
@@ -1,10 +1,13 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
+scalar DateTime
 type Note {
   id: ID!
   content: String!
   author: String!
+  createdAt: DateTime!
+  updatedAt: DateTime!
 }
 type Query {
   hello: String
